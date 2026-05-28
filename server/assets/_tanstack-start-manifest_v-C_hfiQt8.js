0968b120653f23334d04053c5332a694a6eb971a
legend <moengkarabo607@gmail.com>
2026-05-29 00:56:36 +0200

    Updates

--- server/assets/_tanstack-start-manifest_v-C_hfiQt8.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Karabo Moeng/OneDrive/Desktop/Documentos/Gumroad pay/karabo-cape-cuts-main/src/routes/__root.tsx", children: ["/"], preloads: ["/karabo-cape-cuts-main/assets/index-BUnv5F5c.js"] }, "/": { filePath: "C:/Users/Karabo Moeng/OneDrive/Desktop/Documentos/Gumroad pay/karabo-cape-cuts-main/src/routes/index.tsx", children: void 0, preloads: ["/karabo-cape-cuts-main/assets/index-CHQTZSk7.js"] } }, clientEntry: "/karabo-cape-cuts-main/assets/index-BUnv5F5c.js" });
-export {
-  tsrStartManifest
-};
